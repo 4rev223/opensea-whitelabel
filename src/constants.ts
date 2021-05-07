0906194e2ae94dc6f766ef06bf5202889dd989b4
4rev223 <83750999+4rev223@users.noreply.github.com>
2021-05-07 11:42:21 +0300

    test

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,10 @@
 export const ASSET_URL =
+  "https://opensea.io/assets/0x1301566b3cb584e550a02d09562041ddc4989b91/28",
   "https://opensea.io/assets/0x1301566b3cb584e550a02d09562041ddc4989b91/28";
 
 export enum EmbedTheme {
   Default = "true",
-  Simple = "simple",
+  Simple = "night",
 }
 
 export const CHOSEN_THEME = EmbedTheme.Default;
